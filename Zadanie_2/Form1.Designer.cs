da2b4680ed08df6a971a46d1236f2555e4c714a9
DmitriySurnov <79646813832@yandex.ru>
2022-10-21 01:49:15 +0300

    Внесение поправок к второму заданию

--- Zadanie_2/Form1.Designer.cs
+++ Zadanie_2/Form1.Designer.cs
@@ -42,11 +42,11 @@
             this.colElementToolStripLabel = new System.Windows.Forms.ToolStripLabel();
             this.col2ElementToolStripLabel = new System.Windows.Forms.ToolStripLabel();
             this.listBox2 = new System.Windows.Forms.ListBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.openToolStripItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.backContextToolStripItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.menuStrip1.SuspendLayout();
             this.toolStrip1.SuspendLayout();
             this.contextMenuStrip1.SuspendLayout();
@@ -63,8 +63,8 @@
             this.toolStripTextBox2});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(12, 4, 0, 4);
-            this.menuStrip1.Size = new System.Drawing.Size(434, 36);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(24, 8, 0, 8);
+            this.menuStrip1.Size = new System.Drawing.Size(868, 52);
             this.menuStrip1.TabIndex = 4;
             this.menuStrip1.Text = "Проводник";
             // 
@@ -76,7 +76,7 @@
             this.backToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("backToolStripMenuItem.Image")));
             this.backToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.backToolStripMenuItem.Name = "backToolStripMenuItem";
-            this.backToolStripMenuItem.Size = new System.Drawing.Size(36, 28);
+            this.backToolStripMenuItem.Size = new System.Drawing.Size(36, 36);
             this.backToolStripMenuItem.Text = "назад";
             this.backToolStripMenuItem.Click += new System.EventHandler(this.BackToolStripMenuItem_Click);
             // 
@@ -86,7 +86,7 @@
             this.openToolStripMenuItem,
             this.backToglToolStripMenuItem});
             this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
-            this.menuToolStripMenuItem.Size = new System.Drawing.Size(63, 28);
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(114, 36);
             this.menuToolStripMenuItem.Text = "Главная";
             this.menuToolStripMenuItem.Click += new System.EventHandler(this.MenuToolStripMenuItem_Click);
             // 
@@ -94,7 +94,7 @@
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
             this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(293, 38);
             this.openToolStripMenuItem.Text = "Открыть";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
             // 
@@ -102,7 +102,7 @@
             // 
             this.backToglToolStripMenuItem.Name = "backToglToolStripMenuItem";
             this.backToglToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Z)));
-            this.backToglToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.backToglToolStripMenuItem.Size = new System.Drawing.Size(293, 38);
             this.backToglToolStripMenuItem.Text = "Назад";
             this.backToglToolStripMenuItem.Click += new System.EventHandler(this.BackToolStripMenuItem_Click);
             // 
@@ -113,7 +113,7 @@
             this.pathToolStripTextBox.Margin = new System.Windows.Forms.Padding(0, 0, 1, 0);
             this.pathToolStripTextBox.Name = "pathToolStripTextBox";
             this.pathToolStripTextBox.ReadOnly = true;
-            this.pathToolStripTextBox.Size = new System.Drawing.Size(300, 28);
+            this.pathToolStripTextBox.Size = new System.Drawing.Size(596, 39);
             this.pathToolStripTextBox.Visible = false;
             // 
             // toolStripTextBox1
@@ -122,7 +122,7 @@
             this.toolStripTextBox1.Margin = new System.Windows.Forms.Padding(0, 0, 1, 0);
             this.toolStripTextBox1.Name = "toolStripTextBox1";
             this.toolStripTextBox1.ReadOnly = true;
-            this.toolStripTextBox1.Size = new System.Drawing.Size(796, 28);
+            this.toolStripTextBox1.Size = new System.Drawing.Size(1588, 39);
             this.toolStripTextBox1.Text = "12";
             this.toolStripTextBox1.Visible = false;
             // 
@@ -132,7 +132,7 @@
             this.toolStripTextBox2.Margin = new System.Windows.Forms.Padding(0, 0, 1, 0);
             this.toolStripTextBox2.Name = "toolStripTextBox2";
             this.toolStripTextBox2.ReadOnly = true;
-            this.toolStripTextBox2.Size = new System.Drawing.Size(796, 28);
+            this.toolStripTextBox2.Size = new System.Drawing.Size(1588, 39);
             this.toolStripTextBox2.Visible = false;
             // 
             // toolStrip1
@@ -142,17 +142,17 @@
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.colElementToolStripLabel,
             this.col2ElementToolStripLabel});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 426);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 832);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 2, 0);
-            this.toolStrip1.Size = new System.Drawing.Size(434, 25);
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 4, 0);
+            this.toolStrip1.Size = new System.Drawing.Size(868, 35);
             this.toolStrip1.TabIndex = 5;
             this.toolStrip1.Text = "toolStrip1";
             // 
             // colElementToolStripLabel
             // 
             this.colElementToolStripLabel.Name = "colElementToolStripLabel";
-            this.colElementToolStripLabel.Size = new System.Drawing.Size(79, 22);
+            this.colElementToolStripLabel.Size = new System.Drawing.Size(162, 32);
             this.colElementToolStripLabel.Text = "Элементов: 0";
             // 
             // col2ElementToolStripLabel
@@ -160,7 +160,7 @@
             this.col2ElementToolStripLabel.Margin = new System.Windows.Forms.Padding(240, 1, 0, 2);
             this.col2ElementToolStripLabel.Name = "col2ElementToolStripLabel";
             this.col2ElementToolStripLabel.Overflow = System.Windows.Forms.ToolStripItemOverflow.Never;
-            this.col2ElementToolStripLabel.Size = new System.Drawing.Size(79, 22);
+            this.col2ElementToolStripLabel.Size = new System.Drawing.Size(162, 32);
             this.col2ElementToolStripLabel.Text = "Элементов: 0";
             // 
             // listBox2
@@ -168,29 +168,15 @@
             this.listBox2.ContextMenuStrip = this.contextMenuStrip1;
             this.listBox2.Dock = System.Windows.Forms.DockStyle.Right;
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(151, 36);
-            this.listBox2.Margin = new System.Windows.Forms.Padding(6);
+            this.listBox2.ItemHeight = 25;
+            this.listBox2.Location = new System.Drawing.Point(306, 52);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(12, 12, 12, 12);
             this.listBox2.Name = "listBox2";
             this.listBox2.ScrollAlwaysVisible = true;
-            this.listBox2.Size = new System.Drawing.Size(283, 390);
+            this.listBox2.Size = new System.Drawing.Size(562, 780);
             this.listBox2.TabIndex = 7;
             this.listBox2.Click += new System.EventHandler(this.ListBox2_Click);
             this.listBox2.DoubleClick += new System.EventHandler(this.ListBox2_DoubleClick);
-            // 
-            // listBox1
-            // 
-            this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
-            this.listBox1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(0, 36);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(6);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.ScrollAlwaysVisible = true;
-            this.listBox1.Size = new System.Drawing.Size(131, 390);
-            this.listBox1.TabIndex = 6;
-            this.listBox1.Click += new System.EventHandler(this.ListBox1_Click);
-            this.listBox1.DoubleClick += new System.EventHandler(this.ListBox1_DoubleClick);
             // 
             // contextMenuStrip1
             // 
@@ -200,43 +186,59 @@
             this.toolStripSeparator1,
             this.backContextToolStripItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(165, 54);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(270, 82);
             this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.ContextMenuStrip1_Opening);
             // 
             // openToolStripItem
             // 
             this.openToolStripItem.Name = "openToolStripItem";
             this.openToolStripItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-            this.openToolStripItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripItem.Size = new System.Drawing.Size(269, 36);
             this.openToolStripItem.Text = "Открыть";
             this.openToolStripItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(161, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(266, 6);
             // 
             // backContextToolStripItem
             // 
             this.backContextToolStripItem.Name = "backContextToolStripItem";
             this.backContextToolStripItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Z)));
-            this.backContextToolStripItem.Size = new System.Drawing.Size(164, 22);
+            this.backContextToolStripItem.Size = new System.Drawing.Size(269, 36);
             this.backContextToolStripItem.Text = "Назад";
             this.backContextToolStripItem.Click += new System.EventHandler(this.BackToolStripMenuItem_Click);
             // 
+            // listBox1
+            // 
+            this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(0, 52);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(12, 12, 12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(258, 780);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.Click += new System.EventHandler(this.ListBox1_Click);
+            this.listBox1.DoubleClick += new System.EventHandler(this.ListBox1_DoubleClick);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 451);
+            this.ClientSize = new System.Drawing.Size(868, 867);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.menuStrip1);
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.MinimumSize = new System.Drawing.Size(450, 300);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(874, 511);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Проводник";
             this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
